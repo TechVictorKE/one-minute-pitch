57cf60d072b4dece8771e022ecf530ddf924510b
TechVictorKE <victor.maina@student.moringaschool.com>
2021-06-16 15:59:35 +0300

    Changed db_url

--- config.py
+++ config.py
@@ -15,7 +15,9 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','')
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', "postgresql://", 1)
 
 class DevConfig(Config):
     DEBUG = True
